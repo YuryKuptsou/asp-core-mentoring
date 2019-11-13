a7627f16c0c1ee98b56d2f76750f167b54fb4e54
Yury Kuptsou <hllf22@gmail.com>
2019-11-13 23:01:27 +0300

    add tag and html helpers

---
+++ src/AspCore/NorthwindWeb/HtmlHelpers/NorthwindImageLink.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace NorthwindWeb.HtmlHelpers
+{
+    public static class NorthwindImageLinkHelper
+    {
+        private static readonly string _imageWidth = "100";
+        private static readonly string _imageHeight = "100";
+
+        public static HtmlString NorthwindImageLink(this IHtmlHelper html, string imageId)
+        {
+            var urlHelper = new UrlHelper(html.ViewContext);
+            var url = urlHelper.RouteUrl("image", new { id = imageId });
+
+            var img = new TagBuilder("img");
+            img.Attributes.Add("width", _imageWidth);
+            img.Attributes.Add("height", _imageHeight);
+            img.Attributes.Add("src", url);
+
+            var a = new TagBuilder("a");
+            a.Attributes.Add("href", url);
+            a.InnerHtml.AppendHtml(img);
+
+            var writer = new StringWriter();
+            a.WriteTo(writer, HtmlEncoder.Default);
+
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
---
+++ src/AspCore/NorthwindWeb/TagHelpers/NorthwindImageLinkTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindWeb.TagHelpers
+{
+    public class NorthwindImageLinkTagHelper : TagHelper
+    {
+        private readonly IUrlHelperFactory _urlHelperFactory;
+        private readonly string _imageWidth = "100";
+        private readonly string _imageHeight = "100";
+
+
+        public NorthwindImageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            _urlHelperFactory = helperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public int ImageId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+            var url = urlHelper.RouteUrl("image", new { id = ImageId });
+
+            output.TagName = "a";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("href", url);
+            output.Content.SetHtmlContent($"<img width='{_imageWidth}' height='{_imageHeight}' src='{url}' />");
+
+        }
+    }
+}
